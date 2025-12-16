d78fe79ea5fa5178b8bae4c7c77e7763c5b18162
Narmina <qnk95k2w5f@privaterelay.appleid.com>
2025-12-16 16:51:49 +0500

    Refactor Viewer class with new variable names

--- src/Viewer.java
+++ src/Viewer.java
@@ -1,37 +1,57 @@
 public class Viewer {
-    private String name;
-    private int age;
-    private boolean hasVIPMembership;
-    private String phone;
-
-
-    public Viewer(String name, int age, boolean hasVIPMembership, String phone) {
-        this.name = name;
-        this.age = age;
-        this.hasVIPMembership = hasVIPMembership;
-        this.phone = phone;
+    private String imya;
+    private int vozrast;
+    private boolean vip;
+    private String telefon;
+    
+    public Viewer(String i, int v, boolean vipka, String t) {
+        imya = i;
+        vozrast = v;
+        vip = vipka;
+        telefon = t;
     }
-
-
-    public String getName() {
-        return name;
+    
+    public String getImya() {
+        return imya;
     }
-
-    public int getAge() {
-        return age;
+    
+    public int getVozrast() {
+        return vozrast;
     }
-
-    public boolean isVIP() {
-        return hasVIPMembership;
+    
+    public boolean isVip() {
+        return vip;
     }
-    public String getPhone() {
-        return phone;
+    
+    public String getTelefon() {
+        return telefon;
     }
-
-    public void displayInfo() {
-        System.out.println("Зритель: " + name);
-        System.out.println("Возраст: " + age);
-        System.out.println("Номер телефона: "+ phone);
-        System.out.println("VIP: " + (hasVIPMembership ? "Да" : "Нет"));
+    
+    public void setImya(String i) {
+        imya = i;
+    }
+    
+    public void setVozrast(int v) {
+        vozrast = v;
+    }
+    
+    public void setVip(boolean v) {
+        vip = v;
+    }
+    
+    public void setTelefon(String t) {
+        telefon = t;
+    }
+    
+    public void pokazat() {
+        System.out.println("Зритель: " + imya);
+        System.out.println("Возраст: " + vozrast);
+        System.out.println("Телефон: " + telefon);
+        
+        if (vip == true) {
+            System.out.println("VIP статус: Да");
+        } else {
+            System.out.println("VIP статус: Нет");
+        }
     }
 }
